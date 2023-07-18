0729f88737791b336c1dcd2120d7b08b6e5d4031
spancavil <soancavil@gmail.com>
2023-07-18 17:14:19 -0300

    template clase 10

---
+++ Clase 10 - Tab Navigation/my-ecommerce/src/Screens/Home.jsx
@@ -0,0 +1,29 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { colors } from '../Global/Colors'
+import categories from '../Data/categories.json'
+import CategoryItem from '../Components/CategoryItem'
+
+const Home = ({
+  navigation
+}) => {
+  return (
+    <View style={styles.container}>
+        <FlatList
+            data = {categories}
+            keyExtractor={category => category}
+            renderItem={({item}) => <CategoryItem item={item} navigation = {navigation}/>}
+            showsVerticalScrollIndicator={false}
+        />
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.lightPink,
+        alignItems: 'center',
+    }
+})
